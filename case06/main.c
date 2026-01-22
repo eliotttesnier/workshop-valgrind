4776b2739689c1cbbdb9dff1412dd732c6844093
eliotttesnier <eliott.tesnier@epitech.eu>
2026-01-22 15:27:02 +0100

    feat(case06): add main program and exec script for valgrind testing

---
+++ case06/main.c
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2026
+** workshop-valgrind
+** File description:
+** main
+*/
+
+#include <stdlib.h>
+
+int main(void)
+{
+    char *str;
+    int len;
+
+    if (str) {
+        len += 1;
+    }
+    return len;
+}
